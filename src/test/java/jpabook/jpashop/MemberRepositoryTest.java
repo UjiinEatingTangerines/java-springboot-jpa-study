684908236dc91da3ba6f6d916f188f3c592f081c
1996.devjiinu@gmail.com <1996.devjiinu@gmail.com>
2022-06-03 11:19:10 +0900

    22.06.03 title: JPA 기본 세팅
    application.yml
    1. loggin - level - org.hibernate.type: trace 를 추가하면 로그를 찍어볼 수 있다.
    
    build.gradle
    1. 기본 로그는 넘어가는 파라미터를 '?'로 표기하여 갯수 확인 이외는 불가하다.
    2. 라이브러리를 추가하여 해당 이슈를 해결할 수 있는데, 여기서 사용할 라이브러리는 P6Spy 이다.
    
    MemberRepositoryTest.java
    1. @Transactional는 Test(JUnit)에서 사용할 경우, DB 이력을 다시 롤백 시키다.
    2. 해당 로직이 정상 작동했는지 알기 위하여,@Rollback 의 옵션 false를 사용하여 DB에 쌓이는 것을 확인 할 수 있다.

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -13,6 +14,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(value = false)
     public void testMember() throws Exception{
         //given
         Member member = new Member();
@@ -25,6 +27,7 @@ class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
 }
